4b565c95ef62b4e0fb60e1583d9129ac70b22f67
Snehit Sah <48585950+snehitsah@users.noreply.github.com>
2020-05-14 13:19:38 +0530

    Added test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
     println!("Hello, world!");
+
+    println!("{}", addfive(67));
+}
+
+fn addfive(ip: i32) -> i32 {
+    ip + 3
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn exploration() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn another() {
+        assert_eq!(addfive(89), 92);
+    }
 }
